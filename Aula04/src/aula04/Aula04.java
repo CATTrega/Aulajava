6ad87667dad7eff4aa9268d4771b3b6035d39ae9
CATTrega <141193492+CATTrega@users.noreply.github.com>
2023-08-14 20:20:55 -0300

    Add files via upload

---
+++ Aula04/src/aula04/Aula04.java
@@ -0,0 +1,31 @@
+
+package aula04;
+
+/**
+ * Aula Sardinha
+ *Data14/08/2023
+ * @author Clara Amorim
+ */ 
+public class Aula04 {
+
+    public static void main(String[] args) {
+       int a = 10;
+       String b = "10";
+       int c = 2; 
+       String d = "10";
+       
+       
+       int resultadoA = a + c;
+       int resultadoB = a - c;
+       int resultadoC = a * c;
+       int resultadoD = a / c;
+       int resultadoE = a % c;
+       
+       System.out.println (resultadoA);
+       System.out.println (resultadoB);
+       System.out.println (resultadoC);
+       System.out.println (resultadoD);
+       System.out.println (resultadoE);
+    }
+    
+}
